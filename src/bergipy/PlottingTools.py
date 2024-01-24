17bc29b0d34da470b3353aa4006b28c234c827ab
Michael Berghold <michael.berghold@oewf.org>
2024-01-24 23:49:19 +0100

    Initial commit

--- src/bergipy/PlottingTools.py
+++ src/bergipy/PlottingTools.py
@@ -4,6 +4,7 @@ import matplotlib.tri as tri
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from cycler import cycler
 import numpy as np
+import scipy
 
 plt.style.use('dark_background')
 rcParams['figure.figsize'] = (9, 5)
@@ -18,21 +19,16 @@ def custom_cmap(n: int, name: str = 'hsv'):
     return plt.cm.get_cmap(name, n)
 
 
-# TODO: Improve!!!
-def irregular_2d_plot(x, y, z, x_label='', y_label='', title=None, interpolation=True):
+def irregular_2d_plot(x, y, z,
+                      x_label=None, y_label=None, title=None,
+                      interpolation=True, smooth=False,
+                      n_x_intervals=35, n_y_intervals=20):
     x = np.array(x)
     y = np.array(y)
-    sorted_x = np.unique(x)
-    sorted_y = np.unique(y)
+    z = np.array(z)
 
-    dx = np.min(np.diff(sorted_x))
-    x_range = np.arange(sorted_x[0], sorted_x[-1] + dx, dx)
-
-    dy = np.min(np.diff(sorted_y))
-    y_range = np.arange(sorted_y[0], sorted_y[-1] + dy, dy)
-
-    x_range = sorted_x
-    y_range = sorted_y
+    x_range, x_step = np.linspace(np.min(x), np.max(x), n_x_intervals, retstep=True)
+    y_range, y_step = np.linspace(np.min(y), np.max(y), n_y_intervals, retstep=True)
 
     if interpolation:
         triang = tri.Triangulation(x, y)
@@ -41,45 +37,55 @@ def irregular_2d_plot(x, y, z, x_label='', y_label='', title=None, interpolation
         array_2d = interpolator(X, Y)
 
     else:
-        array_2d = np.zeros((len(x_range), len(y_range)))
-        print(array_2d.shape)
+        array_2d = np.zeros((n_x_intervals, n_y_intervals))
 
         for _x, row in enumerate(array_2d):
             for _y, elem in enumerate(row):
-                #measures_values_ids = np.where( (np.abs(x - x_range[_x]) < 1e-12) & (np.abs(y - y_range[_y]) < 1e-12) )[0]
-                measures_values_ids = np.where((x == x_range[_x]) & (y == y_range[_y]))[0]
+                measures_values_ids = np.where( (np.abs(x - x_range[_x]) < x_step / 2) &
+                                                (np.abs(y - y_range[_y]) < y_step / 2) )[0]
 
                 if len(measures_values_ids) == 0:
                     array_2d[_x, _y] = np.nan
                 elif len(measures_values_ids) == 1:
                     array_2d[_x, _y] = z[measures_values_ids[0]]
                 else:
-                    array_2d[_x, _y] = np.mean(np.array(z)[measures_values_ids])
+                    array_2d[_x, _y] = np.mean(z[measures_values_ids])
 
         array_2d = array_2d.T
 
-    # array_2d = scipy.ndimage.gaussian_filter(array_2d, sigma=2, radius=1)
+    if smooth:
+        V = array_2d.copy()
+        V[np.isnan(array_2d)] = 0
+        VV = scipy.ndimage.gaussian_filter(V, sigma=smooth)
 
-    scatter_x = np.interp(x, (min(x), max(x)), (0, len(x_range) - 1))
-    scatter_y = np.interp(y, (min(y), max(y)), (0, len(y_range) - 1))
+        W = np.ones(array_2d.shape)
+        W[np.isnan(array_2d)] = 0
+        WW = scipy.ndimage.gaussian_filter(W, sigma=smooth)
+
+        WW[WW == 0] = np.nan
+        array_2d = VV / WW
+
+    scatter_x = np.interp(x, (np.min(x), np.max(x)), (0, len(x_range) - 1))
+    scatter_y = np.interp(y, (np.min(y), np.max(y)), (0, len(y_range) - 1))
 
     fig, ax = plt.subplots()
 
-    im_plot = ax.imshow(array_2d, origin='lower')
+    im_plot = ax.imshow(array_2d, origin='lower', aspect='auto')
     plt.title(title)
 
     ax.scatter(scatter_x, scatter_y, marker='x', color='red', s=1)
 
-    for x, y in zip(scatter_x, scatter_y):
-        occurrences = list(zip(scatter_x, scatter_y)).count((x, y))
-        ax.annotate(occurrences, (x, y), color='white')
+    for _x, _y in zip(scatter_x, scatter_y):
+        occurrences = list(zip(scatter_x, scatter_y)).count((_x, _y))
+        ax.annotate(occurrences, (_x, _y), color='white')
 
-    # nx = max(1, len(x_range)//6)
-    # ny = max(1, len(y_range)//6)
-    nx = None
-    ny = None
-    plt.xticks(range(len(x_range))[::nx], x_range[::nx], rotation=90)
-    plt.yticks(range(len(y_range))[::ny], y_range[::ny])
+    x_decimal_places = max([len(str(_x).split('.')[-1]) for _x in x])
+    y_decimal_places = max([len(str(_y).split('.')[-1]) for _y in y])
+
+    nx = max(1, n_x_intervals//20)
+    ny = max(1, n_y_intervals//15)
+    plt.xticks(range(len(x_range))[::nx], [round(_x, x_decimal_places) for _x in x_range[::nx]], rotation=90)
+    plt.yticks(range(len(y_range))[::ny], [round(_y, y_decimal_places) for _y in y_range[::ny]])
     plt.xlabel(x_label)
     plt.ylabel(y_label)
 
