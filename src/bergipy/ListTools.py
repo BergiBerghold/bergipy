669b2016643572c3890ef07fc943699a844acf35
Michael Berghold <michael.berghold@oewf.org>
2024-02-13 19:50:53 +0100

    Initial commit

--- src/bergipy/ListTools.py
+++ src/bergipy/ListTools.py
@@ -32,6 +32,26 @@ def hilbertizer(input_list):
     return output_list
 
 
+def hilbertizer_new(input_array: np.ndarray):
+    if not isinstance(input_array, np.ndarray):
+        raise TypeError('Input must be Numpy array')
+
+    array_shape = np.array(input_array.shape)
+    array_dimensions = len(input_array.shape)
+
+    n_bits_necessary = len(format(np.max(array_shape) - 1, 'b'))
+    format_specifier = f'0{n_bits_necessary}b'
+
+    for flattened_idx in range(2**n_bits_necessary * array_dimensions):
+        binary = format(flattened_idx, format_specifier)
+        reversed_binary = binary[::-1]
+
+        array_indices = np.array([int(reversed_binary[offset::array_dimensions], 2) for offset in range(array_dimensions)])
+        if (array_indices >= array_shape).any(): continue
+
+        yield input_array[tuple(array_indices)]
+
+
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
--- src/bergipy/__init__.py
+++ src/bergipy/__init__.py
@@ -1,5 +1,5 @@
-from .ListTools import hilbertizer, find_nearest, find_latest
+from .ListTools import hilbertizer, hilbertizer_new, find_nearest, find_latest
 from .IMX219Tools import dec2reg, array2png, png2array
-from .PlottingTools import custom_cmap, irregular_2d_plot
+from .PlottingTools import custom_cmap, irregular_2d_plot, use_plotting_defaults
 from .ImageTools import get_subpixel
 
