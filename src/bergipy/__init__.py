d0bae2113b3a7fef6b32c106c20a8a8169119ff6
Michael Berghold <michael.berghold@oewf.org>
2024-02-14 12:00:55 +0100

    Upgraded hilbertizer

--- src/bergipy/__init__.py
+++ src/bergipy/__init__.py
@@ -1,4 +1,4 @@
-from .ListTools import hilbertizer, hilbertizer_new, find_nearest, find_latest
+from .ListTools import hilbertizer, find_nearest, find_latest
 from .IMX219Tools import dec2reg, array2png, png2array
 from .PlottingTools import custom_cmap, irregular_2d_plot, use_plotting_defaults
 from .ImageTools import get_subpixel
