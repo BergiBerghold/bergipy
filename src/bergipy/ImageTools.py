6934051916c7c10a48475d67192438e57b0e137e
Michael Berghold <michael.berghold@oewf.org>
2024-04-08 19:40:31 +0200

    Added grayscale_to_rgb funct

--- src/bergipy/ImageTools.py
+++ src/bergipy/ImageTools.py
@@ -2,13 +2,17 @@ import numpy as np
 import cv2
 
 
-def get_subpixel(img, x, y):
+def get_subpixel(image, x, y):
     x -= 0.5
     y -= 0.5
 
-    patch = cv2.getRectSubPix(img, (1,1), (x, y), np.zeros((1,1)), cv2.CV_32F)
+    patch = cv2.getRectSubPix(image, (1,1), (x, y), np.zeros((1,1)), cv2.CV_32F)
 
     if patch is not None:
         return patch[0][0]
 
     return None
+
+
+def grayscale_to_rgb(image):
+    return np.stack((image,) * 3, axis=-1)
