a5c97ede1a2a77358ef051e431efe713f326e911
Michael Berghold <michael.berghold@oewf.org>
2024-04-16 15:59:12 +0200

    Added frame unpacker for raw10 pixel format

--- src/bergipy/IMX219Tools.py
+++ src/bergipy/IMX219Tools.py
@@ -13,3 +13,67 @@ def array2png(array, filename):
 
 def png2array(filename):
     return cv2.imread(filename, cv2.IMREAD_UNCHANGED) >> 6
+
+
+def unpack_raw10(bytestream, rows=2464, columns=3280, chunks=820, throwaway_bits=28):
+    '''
+    Examples for different modes of the IMX219:
+
+    Full frame (default):
+        rows = 2464
+        columns = 3280
+        chunks = 820
+        throwaway_bits = 28
+
+    2x2 Binning:
+        rows = 1232
+        columns = 3280
+        chunks = 820
+        throwaway_bits = 28
+
+    4x4 Binning:
+        rows = 616
+        columns = 3280
+        chunks = 820
+        throwaway_bits = 28
+
+    1920x1080 Windowing:
+        rows = 1088
+        columns = 1920
+        chunks = 480
+        throwaway_bits = 0
+
+    256x256 Windowing:
+        rows = 256
+        columns = 256
+        chunks = 64
+        throwaway_bits = 0
+
+    128x128 Windowing:
+        rows = 128
+        columns = 128
+        chunks = 32
+        throwaway_bits = 0
+    '''
+
+    expected_size = rows * (chunks * 5 + throwaway_bits)
+    bytestream = bytestream[:expected_size]
+
+    data_array = np.frombuffer(bytestream, dtype=np.uint8)
+    data_array = data_array.reshape((rows, chunks * 5 + throwaway_bits))
+
+    if throwaway_bits: data_array = data_array[:, :-throwaway_bits]
+    data_array = data_array.reshape((rows, chunks, 5))
+
+    data_array = data_array.astype(np.uint16)
+    data_array[:, :, :-1] = data_array[:, :, :-1] << 2
+
+    data_array[:, :, 3] |= data_array[:, :, 4] & 3
+    data_array[:, :, 2] |= (data_array[:, :, 4] >> 2) & 3
+    data_array[:, :, 1] |= (data_array[:, :, 4] >> 4) & 3
+    data_array[:, :, 0] |= (data_array[:, :, 4] >> 6)
+
+    data_array = data_array[:, :, :4]
+    data_array = data_array.reshape((rows, columns))
+
+    return data_array
